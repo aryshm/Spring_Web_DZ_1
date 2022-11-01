965284d760c968dcf3c8cc357cc3aacc6a7da2db
aryshm <arishensky@yandex.ru>
2022-11-01 10:19:23 +0400

    New features

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,37 @@
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-        Server server = new Server();
-        server.start();
+        final var server = new Server();
+        server.addHandler("GET", "/index.html", (request, outputStreamStream) -> {
+            try {
+                Server.positiveresponse(request, outputStreamStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+//
+//
+//
+//        for (int i = 0; i < ValidPaths.validPaths.size(); i++) {
+//            server.addHandler("GET", ValidPaths.validPaths.get(i), (request, outputStreamStream) -> {
+//                try {
+//                    Server.positiveresponse(request, outputStreamStream);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            });
+//        }
+
+        server.addHandler("POST", "/links.html", (request, outputStreamStream) -> {
+            try {
+                Server.positiveresponse(request, outputStreamStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        server.listen(9999);
     }
 }
---
+++ src/main/java/Request.java
@@ -0,0 +1,26 @@
+import java.io.InputStream;
+import java.util.Map;
+
+public class Request {
+
+    private final String method;
+    private final String path;
+    private final Map<String, String> headers;
+    private final InputStream body;
+
+    public Request(String method, String path, Map<String, String> headers, InputStream body) {
+        this.method = method;
+        this.path = path;
+        this.headers = headers;
+        this.body = body;
+    }
+
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+}
